aaabce72f61ed8dd016a3ea0105b12c9f9c2b45d
mohibiology <abdulmohib1408@gmail.com>
2025-07-04 19:00:02 +0500

    Setup leaderboard model, serializers, and APIs

--- leaderboard_project/urls.py
+++ leaderboard_project/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('your_app_name.urls')),
+    path('', include('leaderboard.urls')),
 ]
